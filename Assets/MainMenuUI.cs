8f60b7e2520d0adccbc7ad4965b7285d3fcf6c3f
Sainithin1601 <sainithin1601@gmail.com>
2026-02-18 16:23:37 -0500

    Main menu buttons working
    connected play and quit buttons

--- Assets/MainMenuUI.cs
+++ Assets/MainMenuUI.cs
@@ -5,6 +5,18 @@ public class MainMenuUI : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("Psyche Scene");
+        SceneManager.LoadScene("Psyche Scene"); // Make sure name matches exactly
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT BUTTON CLICKED");
+
+        Application.Quit();
+
+        // This makes quit work inside Unity Editor too
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }
