ffb44480a83104d4765e4ed6b43fc4ef988bf482
Curelyshot <nathanesber18@gmail.com>
2026-03-16 12:01:31 -0400

    Bug Fixes
    Fixed the bug where the upgrade buttons was not working (added one) and fixed the bug where all drills required the correct amount to place, but when placing would always subtract 250.

--- Assets/Script/Drill/Drill.cs
+++ Assets/Script/Drill/Drill.cs
@@ -24,6 +24,7 @@ public class Drill : MonoBehaviour
     {
         ApplySpeedStats();
         ApplyDepthStats();
+	placementCost  = DrillCost.Instance.GetNextDrillCost();
     }
 
     void Update()
