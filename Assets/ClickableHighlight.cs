c46bd802af7d3b3541026c465b6ea9fa54e06f2a
Yaya3124 <125766626+Yaya3124@users.noreply.github.com>
2026-04-06 16:14:17 -0400

    All 5 spaceship level upgrades
    All upgrades done, but I am trying to fix the the size of the last two upgrades.

--- Assets/ClickableHighlight.cs
+++ Assets/ClickableHighlight.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using UnityEngine.UI;
 
 public class ClickableHighlight : MonoBehaviour
 {
@@ -7,28 +6,55 @@ public class ClickableHighlight : MonoBehaviour
     public Color highlightColor = new Color(1f, 0.92f, 0.016f, 1f);
     private Color originalColor;
 
-    [Header("Upgrade Settings")]
-    public Sprite upgradedSprite;
+    [Header("Upgrade Sprites (Level 2 to 5)")]
+    public Sprite level2Sprite;
+    public Sprite level3Sprite;
+    public Sprite level4Sprite;
+    public Sprite level5Sprite;
+
+    [Header("Popup")]
     public GameObject upgradePopup;
 
+    private int currentLevel = 1;
+
     void Start()
-    {
-        sr = GetComponent<SpriteRenderer>();
-        originalColor = sr.color;
-        upgradePopup.SetActive(false);
-    }
+{
+    sr = GetComponent<SpriteRenderer>();
+    originalColor = sr.color;
+    upgradePopup.SetActive(false); // this already disables the whole canvas
+}
+
 
     void OnMouseEnter() { sr.color = highlightColor; }
     void OnMouseExit()  { sr.color = originalColor; }
 
-    void OnMouseDown()
+   void Update()
+{
+    if (Input.GetMouseButtonDown(0))
     {
-        upgradePopup.SetActive(true);
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(mousePos, Vector2.zero);
+        
+        if (hit.collider != null && hit.collider.gameObject == gameObject)
+        {
+            if (currentLevel < 5)
+                upgradePopup.SetActive(true);
+        }
     }
+}
 
     public void OnYesClicked()
     {
-        sr.sprite = upgradedSprite;
+        currentLevel++;
+
+        switch (currentLevel)
+        {
+            case 2: sr.sprite = level2Sprite; break;
+            case 3: sr.sprite = level3Sprite; break;
+            case 4: sr.sprite = level4Sprite; break;
+            case 5: sr.sprite = level5Sprite; break;
+        }
+
         upgradePopup.SetActive(false);
         sr.color = originalColor;
     }
