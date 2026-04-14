63a6baff96644d371b58c523cff5e2be93b58d68
Curely <nathanesber18@gmail.com>
2026-04-14 13:58:14 -0400

    Name Change + Drill Upgrade Bug fix
    Change the main menu name and I also fixed the bug dealing with the upgrade gui

--- Assets/Script/Main Scene Script/Drill/DrillHover.cs
+++ Assets/Script/Main Scene Script/Drill/DrillHover.cs
@@ -8,9 +8,12 @@ public class DrillHover : MonoBehaviour
 
     public Color highlightColor = new Color(1f, 0.92f, 0.016f, 1f);
 
+    private Drill drill;
+
     void Awake()
     {
-        // IMPORTANT: go into the animated child
+        drill = GetComponent<Drill>();
+
         rend = transform.Find("animatedDrill_0")
                         ?.GetComponentInChildren<Renderer>();
 
@@ -23,18 +26,31 @@ public class DrillHover : MonoBehaviour
         if (EventSystem.current.IsPointerOverGameObject())
             return;
 
-        Debug.Log("HOVER ENTER");
-
         if (rend != null)
             rend.material.color = highlightColor;
-        else
-            Debug.LogWarning("Renderer not found");
     }
 
     void OnMouseExit()
     {
         if (rend != null)
             rend.material.color = originalColor;
+    }
+
+    void OnMouseDown()
+    {
+        // Block clicks ONLY if UI is open and you're clicking UI
+        if (DrillUpgradeUI.Instance != null &&
+            DrillUpgradeUI.Instance.gameObject.activeSelf &&
+            EventSystem.current.IsPointerOverGameObject())
+        {
+            return;
+        }
+
+        if (drill != null)
+        {
+            Debug.Log("Drill clicked!");
+            DrillUpgradeUI.Instance.Open(drill);
+        }
     }
 }
 
